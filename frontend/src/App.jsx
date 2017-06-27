0b426403bf1495ba87657750144ad6bb092879ab
Roni Colling <veronica.colling@dwp.gsi.gov.uk>
2017-06-27 10:35:38 +0100

    moved app into separate file

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import AwesomeComponent from './AwesomeComponent.jsx';
+
+class App extends React.Component {
+  render () {
+    return (
+    <div>
+    <p> Hello React cos I am sick of saying hello world!</p>
+    <AwesomeComponent />
+    </div>
+    );
+  }
+}
+
+export default App;
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,16 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-import AwesomeComponent from './AwesomeComponent.jsx';
-
-class App extends React.Component {
-  render () {
-    return (
-    <div>
-    <p> Hello React cos I am sick of saying hello world!</p>
-    <AwesomeComponent />
-    </div>
-    );
-  }
-}
+import App from './App.jsx';
 
 render(<App/>, document.getElementById('app'));
